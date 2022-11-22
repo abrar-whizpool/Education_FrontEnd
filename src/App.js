c2417f3e018e81ff0d37cb2f17cb7f14e816fa62
Abrar <99638100+abrar-whizpool@users.noreply.github.com>
2022-11-22 14:08:55 +0500

    whatsapp added :fire:

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import "./App.style.css";
 import { routes } from "./routes/routes";
-
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 function App() { 
   return (
     <>
         <RouterProvider  router={routes} />
+        <FloatingWhatsApp phoneNumber="03155591241" />
     </>
   );
 }
